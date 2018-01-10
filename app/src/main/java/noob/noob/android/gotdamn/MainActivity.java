a5b1d116b0e10766e723b12a5fbea964fed6e878
Emilio <editrocchio@gmail.com>
2018-01-09 22:09:15 -0800

    Added a "gotdamn" counter.

--- app/src/main/java/noob/noob/android/gotdamn/MainActivity.java
+++ app/src/main/java/noob/noob/android/gotdamn/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.util.Random;
 import android.media.MediaPlayer;
@@ -13,10 +14,12 @@ import android.view.View.OnClickListener;
 
 import com.emilio.noob.R;
 
+import org.w3c.dom.Text;
+
 
 public class MainActivity extends AppCompatActivity {
     private static Random rgenerator = new Random();
-
+    int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,23 +33,32 @@ public class MainActivity extends AppCompatActivity {
         final MediaPlayer stuffToDo = MediaPlayer.create(this, R.raw.gotsufftodo);
         final MediaPlayer mission = MediaPlayer.create(this, R.raw.mission);
 
-        //Listener to play random sound clip when button is pressed.
+        //Listener to play random sound clip when button is pressed, increase gotdamn count if
+        //"gotdamn" is played
         playNoobNoobSound.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                TextView counter = (TextView) findViewById(R.id.textView2);
+
                 Context context = getApplicationContext();
                 int dur = Toast.LENGTH_SHORT;
-
                 Toast.makeText(context, stringGenerator(), dur).show();
 
                 int i = rgenerator.nextInt((5-1) + 1) + 1;
 
                 if (i == 1) {
                     gd1.start();
+                    count++;
+                    counter.setText("GotDamn count: " + count);
                 } else if (i == 2) {
                     gd2.start();
+                    count++;
+                    counter.setText("GotDamn count: " + count);
                 } else if (i == 3) {
                     gd3.start();
+                    count++;
+                    counter.setText("GotDamn count: " + count);
                 } else if (i == 4) {
                     stuffToDo.start();
                 } else if (i == 5) {
@@ -66,4 +78,12 @@ public class MainActivity extends AppCompatActivity {
         String toDisplay = s[rgenerator.nextInt(s.length)];
         return toDisplay;
     }
+
+    //Display and update gotdamn counter
+    public void counter() {
+        int count = 0;
+
+
+    }
+
 }
